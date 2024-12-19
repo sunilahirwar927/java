6c47b6c2d476ae5ea75a3e50aff805a1d5b74e06
sunilahirwar927 <153831940+sunilahirwar927@users.noreply.github.com>
2024-12-19 19:30:24 +0530

    Add files via upload

---
+++ for Loop.java
@@ -0,0 +1,9 @@
+ class Loops{
+    public static void main(String[]args){
+      //  for(int i=0; i<100; i++){
+       // System.out.println("hello world");
+       for(int i=0; i<11; i++){
+       System.out.println( i);
+            }
+         }
+    }
